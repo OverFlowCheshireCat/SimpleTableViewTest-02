bd3abfe9b07fda57c2c4a391c6c23e6832a4c36c
dit03 <dit@imac-4.local>
2019-05-02 16:32:39 +0900

    New Source

--- SimpleTableViewTest 02/ViewController.swift
+++ SimpleTableViewTest 02/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Section Header 1"
+        } else {
+            return "Section Header 2"
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if section == 0 {
+            return "Section Footer 1"
+        } else {
+            return "Section Footer 2"
+        }
+    }
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Re", for: indexPath)
+        let section = indexPath.section
+        let row = indexPath.row
+        
+        cell.textLabel?.text = "Section " + String(section)
+        cell.detailTextLabel?.text = "Row " + String(row)
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
     }
-
-
 }
-
