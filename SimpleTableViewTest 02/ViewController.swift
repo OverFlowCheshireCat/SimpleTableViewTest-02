b43e762f2b3dec3f2bc58a59c496901ef02be264
dit03 <dit@imac-4.local>
2019-05-02 16:32:58 +0900

    Edit Source

--- SimpleTableViewTest 02/ViewController.swift
+++ SimpleTableViewTest 02/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  SimpleTableViewTest 02
-//
-//  Created by dit03 on 2019. 5. 2..
-//  Copyright © 2019년 201730529 김민철. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
