07efa33f132bedb90f18209b90f346939480d155
dit03 <dit@imac-4.local>
2019-05-02 16:22:12 +0900

    Initial Commit

---
+++ SimpleTableViewTest 02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTableViewTest 02
+//
+//  Created by dit03 on 2019. 5. 2..
+//  Copyright © 2019년 201730529 김민철. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
